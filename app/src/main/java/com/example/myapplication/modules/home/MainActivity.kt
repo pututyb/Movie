882913cf1c4c46e84eb3ec385783f5d6f5fed2ca
Putut Yusri Bahtiar <pututyb@gmail.com>
2021-05-10 18:53:45 +0700

    Initial Commit

--- app/src/main/java/com/example/myapplication/modules/home/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/modules/home/MainActivity.kt
@@ -80,10 +80,54 @@ class MainActivity : AppCompatActivity() {
                 "1",
                 "Sonic the Hedgehog",
                 "Based on the global blockbuster videogame franchise from Sega, Sonic the Hedgehog tells the story of the world’s speediest hedgehog as he embraces his new home on Earth. In this live-action adventure comedy, Sonic and his new best friend team up to defend the planet from the evil genius Dr. Robotnik and his plans for world domination.",
-                " Action, Science Fiction, Comedy, Family",
+                "Action, Science Fiction, Comedy, Family",
                 R.drawable.ic_poster_sonic,
                 R.raw.video_sonic,
                 5.0F
+            )
+        )
+        dataList.add(
+            FilmModel(
+                "1",
+                " Mortal Kombat (2021)",
+                "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero, seeks out and trains with Earth's greatest champions as he prepares to stand against the enemies of Outworld in a high stakes battle for the universe.",
+                " Action, Fantasy, Adventure, Science Fiction ",
+                R.drawable.ic_mortal_kombat,
+                R.raw.video_mortal_kombat,
+                4.5F
+            )
+        )
+        dataList.add(
+            FilmModel(
+                "1",
+                "Godzilla vs. Kong (2021)",
+                "In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.",
+                "Science Fiction, Action, Drama ",
+                R.drawable.ic_kingkong,
+                R.raw.video_godzilla,
+                4.5F
+            )
+        )
+        dataList.add(
+            FilmModel(
+                "1",
+                "Nobody (2021)",
+                "Hutch Mansell, a suburban dad, overlooked husband, nothing neighbor — a \"nobody.\" When two thieves break into his home one night, Hutch's unknown long-simmering rage is ignited and propels him on a brutal path that will uncover dark secrets he fought to leave behind.",
+                " Science Fiction, Drama",
+                R.drawable.ic_nobody,
+                R.raw.video_nobody,
+                4.5F
+            )
+        )
+        dataList.add(
+            FilmModel(
+                "1",
+                "Demon Slayer",
+                "Tanjirō Kamado, joined with Inosuke Hashibira, a boy raised by boars who wears a boar's head, and Zenitsu Agatsuma, a scared boy who reveals his true power when he sleeps, boards the Infinity Train on a new mission with the Fire Hashira, Kyōjurō Rengoku, to defeat a demon who has been tormenting the people and killing the demon slayers who oppose it!",
+                "Animation, Action, Adventure, Fantasy, Drama",
+                R.drawable.ic_demon_slayer,
+                R.raw.video_demon_slayer,
+                4.5F
             )
         )
     }
